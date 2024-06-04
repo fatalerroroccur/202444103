1909b5d20b1bd1b7965dd2fade3472837c1c9d53
fatalerroroccur <kdh03580>
2024-06-04 17:21:04 +0900

    new file:   20240604/20240604-1.c 	new file:   20240604/20240604-1.exe 	new file:   20240604/20240604-2.c 	new file:   20240604/20240604-2.exe

---
+++ 20240604/20240604-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    char sa[]="Spacezone";
+    char *ps="Spacezone";
+    int cnt;
+    printf("배열 선언 char sa[] = \"Spacezone\"\n");
+    printf("포인터 선언 char *ps = \"Spacezone\"\n");
+    printf("\n배열 주소값 : %d\n",sa);
+    printf("포인터 주소값 : %d\n",ps);
+    printf("\n배열 사용 \t 포인터 사용\n");
+    for(cnt=0;cnt<9;cnt++){
+        printf("sa[%d] : %c\t\t",cnt, sa[cnt]);
+        printf("*(ps+%d) : %c\n",cnt, *(ps+cnt));
+    }
+}
